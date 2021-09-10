9d756b175b0f67d8153de8f580142bd40b1fb790
Aaron <aarongabriel147@gmail.com>
2021-09-09 19:44:40 -0700

    second go around

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -3,19 +3,36 @@ import {render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import ContactForm from './ContactForm';
+import { ErrorMessage } from 'react-hook-form';
 
 test('renders without errors', () => {
     render(<ContactForm />)
 });
 
+
+
 test('renders the contact form header', ()=> {
     render(<ContactForm />)
-    const formTitle = screen.queryByText('Contact Form');
-    expect(formTitle).toBeInTheDocument();
+    const formTitle = screen.queryByText('Contact Form'); // (/contact form/i) for case insensitivity.
+    expect(formTitle).toBeInTheDocument();                // Tests to see if in document
+    expect(formTitle).toBeTruthy();
+    expect(formTitle).toHaveTextContent(/contact form/i)
 });
+
+
+
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
     render(<ContactForm />)
+    const firstNameField = screen.getByLabelText(/first name*/i)
+    // console.log(firstNameField)
+    userEvent.type(firstNameField, "123");
+
+    // const errorMessages = await screen.findAllByTestId('error');
+    // expect(errorMessages).toHaveLength(1);   
+
+    
+    screen.debug(); // shows you everything in the DOM that the test can see inside render(<ContactForm />)
 });
 
 // test('renders THREE error messages if user enters no values into any fields.', async () => {
